1c93b6bfdcd78cc548d54724e80c8be56debd13a
fgonzalezberro <fgonzalezberro@gmail.com>
2021-03-17 00:25:28 -0300

    Set pokeball-icon animation

--- src/components/seeker/Seeker.js
+++ src/components/seeker/Seeker.js
@@ -6,12 +6,22 @@ import pokeball from '../../assets/img/pokeball.png';
 
 // Seeker Component
 const Seeker = () =>{
+     const setPokeballAnimation = () =>{
+          document.querySelector('.pokeball-icon').style.width = '60px';
+     }
+
+     const deletePokeballAnimation = () =>{
+          document.querySelector('.pokeball-icon').style.width = '40px';
+     }
+
      return(
           <div className='seeker-component'>
                <img className='pokeball-icon' src={pokeball}/>
                <input className = 'pokemon-to-search'
                     type = 'text'
                     placeholder = 'Buscar Pokemon'
+                    onMouseEnter={() => setPokeballAnimation()}
+                    onMouseLeave={() => deletePokeballAnimation()}
                />
           </div>   
      );
