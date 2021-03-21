c4dfb11b2196f09f42677d56c105135233e0cdff
fgonzalezberro <fgonzalezberro@gmail.com>
2021-03-20 21:28:47 -0300

    Install axios and create API req

--- src/components/seeker/Seeker.js
+++ src/components/seeker/Seeker.js
@@ -1,6 +1,9 @@
 // Import React
 import React , {useState} from 'react';
 
+// Import Axios
+import axios from 'axios';
+
 // Import images
 //import charmander from '../../assets/img/charmander.png';
 //import charizard from '../../assets/img/charizard.png';
@@ -14,6 +17,20 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 // Seeker Component
 const Seeker = () =>{
+
+     // Pokemon API URL
+     const API = axios.create({
+          baseURL: 'https://pokeapi.co/api/v2/pokemon/'
+     });
+
+     // Search state
+     const [searchValue , setSearchValue] = useState(null);
+
+     // Request poke api
+     const reqPokeApi = async () =>{
+          const result = await API.get(searchValue);
+          console.log(result.data.name);
+     }
 
      // Seeker animation
      const setPokeballAnimation = () =>{
@@ -32,9 +49,10 @@ const Seeker = () =>{
                     placeholder = 'Pokemon :  Name / ID'
                     onMouseEnter={() => setPokeballAnimation()}
                     onMouseLeave={() => deletePokeballAnimation()} 
+                    onChange={(e) => setSearchValue(e.target.value)}
                />
                <Animated animationIn='bounceIn infinite' animationOut='tada' animationInDuration={3000} animationOutDuration={1500} isVisible={true}>
-                    <FontAwesomeIcon className='search-icon' icon={faSearch} />
+                    <FontAwesomeIcon className='search-icon' icon={faSearch} onClick={() => reqPokeApi()} />
                </Animated>
           </div>   
      );
