2efd9c6272c782edebb24a54800ed5a0ca1c7ed0
fgonzalezberro <fgonzalezberro@gmail.com>
2021-03-27 19:04:43 -0300

    Componentize response data message

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -7,17 +7,14 @@ import {AppContext} from '../provider/provider.js';
 // Import Animated CSS plugin
 import {Animated} from "react-animated-css";
 
-// Import react icons
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart, faRocket, faMeteor, faShieldAlt, faShieldVirus, faBolt, faRunning, faIdCardAlt, faBomb, faViruses} from '@fortawesome/free-solid-svg-icons';
 
 // Import Components
 import ErrorMessage from '../error-message/ErrorMessage';
 import EmptyMessage from '../empty-message/EmptyMessage';
+import ResponseData from '../response-data/ResponseData';
 
 // Import images
 import pokeball from '../../assets/img/pokeball.png';
-import fire from '../../assets/img/attributes-img/fire-type.png';
 import bug from '../../assets/img/attributes-img/bug-type.webp';
 import dark from '../../assets/img/attributes-img/dark-type.png';
 import dragon from '../../assets/img/attributes-img/dragon-type.webp';
@@ -41,85 +38,24 @@ const Dashboard = () => {
     const [searchState , setSeatchState] = useContext(AppContext);
 
     // Destructuring object
-    const {id, hp, atk, def, specialAttack, specialDefense, speed, name, ability, principalMove, type} = searchState;
+    const {reqStatus , error} = searchState;
 
     return (
         <section className='dashboard'>
             <Animated animationIn='pulse infinite' animationOut='rubberBand' animationInDuration={3000} animationOutDuration={1500} isVisible={true}>
-                <img className='master-ball'  src={pokeball}  alt='Pokemon image'/>
+                <img className='master-ball' src={pokeball} alt='Pokemon image'/>
             </Animated>
 
             <div className='data-dashboard'>
-                {searchState.reqStatus ? <PokemonRestul searchState={searchState} />  : <DashboardMessage searchState={searchState}/>}
+                {reqStatus ? <ResponseData />  : <DashboardMessage error={error}/>}
             </div>
         </section>
     );
 };
 
- // Empty search message
- const DashboardMessage = ({searchState}) => (
-    searchState.error ? <ErrorMessage /> : <EmptyMessage />
- );
-
- // Pokemon search result
- const PokemonRestul = ({searchState}) => (  
-    <div className='pokemon-result'>
-        <div className='pokemon-result-data'>
-            <div className='pokemon-name'>
-                <p className='pokemon-result-name'>{searchState.name.charAt(0).toUpperCase() + searchState.name.slice(1)}</p>
-            </div>
-
-            <div className='pokemon-image'>
-                <Animated animationIn='zoomIn' animationOut='heartBeat' animationInDuration={3000} animationOutDuration={1000} isVisible={true}>
-                    <img src={searchState.image} alt='Poke image'/>
-                </Animated>
-
-                <img className='pokemon-attribute-ico' src={fire}/>
-            </div>
-
-            <div className='pokemon-id'> 
-                <p>ID: {searchState.id}</p>
-            </div>
-        </div>
-        
-        <div className='pokemon-result-attributes'>
-            <div className='pokemon-result-attributes-container'>
-                <div className='pokemon-attribute'>
-                    <p><FontAwesomeIcon icon={faHeart}/> HP: {searchState.hp}</p>
-                    <progress className={searchState.type} value={searchState.hp} max='160'>{searchState.hp}</progress>
-                </div>
-                <div className='pokemon-attribute'>
-                    <p><FontAwesomeIcon icon={faRocket}/> Attack: {searchState.atk}</p>
-                    <progress value={searchState.atk} max='160'></progress>
-                </div>
-                <div className='pokemon-attribute'>
-                    <p><FontAwesomeIcon icon={faShieldAlt}/> Defense: {searchState.def}</p>
-                    <progress value={searchState.def} max='160'></progress>
-                </div>
-                <div className='pokemon-attribute'>
-                    <p><FontAwesomeIcon icon={faMeteor}/> Special Attack: {searchState.specialAttack}</p>
-                    <progress value={searchState.specialAttack} max='160'></progress>
-                </div>
-                <div className='pokemon-attribute'>
-                    <p><FontAwesomeIcon icon={faShieldVirus}/> Special Defense: {searchState.specialDefense}</p>
-                    <progress value={searchState.specialDefense} max='160'></progress>
-                </div>
-                <div className='pokemon-attribute'>
-                    <p><FontAwesomeIcon icon={faBolt}/> Speed: {searchState.speed}</p>
-                    <progress value={searchState.speed} max='160'></progress>
-                </div>
-                <div className='pokemon-attribute'>
-                    <p><FontAwesomeIcon icon={faViruses}/> Type: <span className={`${searchState.type} pokemon-type`}>{searchState.type.charAt(0).toUpperCase() + searchState.type.slice(1)}</span></p>
-                </div>
-                <div className='pokemon-attribute'>
-                    <p><FontAwesomeIcon icon={faRunning}/> Principal move: {searchState.principalMove.charAt(0).toUpperCase() + searchState.principalMove.slice(1)}</p>
-                </div>
-                <div className='pokemon-attribute'>
-                    <p><FontAwesomeIcon icon={faBomb}/> Principal ability: {searchState.ability.charAt(0).toUpperCase() + searchState.ability.slice(1)}</p>
-                </div>
-            </div>
-        </div>
-    </div>
+ // Dashboard message
+ const DashboardMessage = ({error}) => (
+    error ? <ErrorMessage /> : <EmptyMessage />
  );
 
 export default Dashboard;
---
+++ src/components/response-data/ResponseData.js
@@ -0,0 +1,85 @@
+// Import react library
+import React , {useContext} from 'react';
+
+// Import react icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart, faRocket, faMeteor, faShieldAlt, faShieldVirus, faBolt, faRunning, faIdCardAlt, faBomb, faViruses} from '@fortawesome/free-solid-svg-icons';
+
+// Import AppContext
+import {AppContext} from '../provider/provider.js';
+
+// Import Animated CSS plugin
+import {Animated} from "react-animated-css";
+
+// Import image
+import fire from '../../assets/img/attributes-img/fire-type.png';
+
+
+const ResponseData = () => {
+    const [searchState , setSeatchState] = useContext(AppContext);
+
+    // Destructuring object
+    const {id, hp, atk, def, specialAttack, specialDefense, speed, name, ability, principalMove, type, image} = searchState;
+
+    return (
+        <div className='pokemon-response'>
+            <div className='pokemon-response-data'>
+                <div className='pokemon-name'>
+                    <p className='pokemon-response-name'>{name.charAt(0).toUpperCase() + name.slice(1)}</p>
+                </div>
+
+                <div className='pokemon-image'>
+                    <Animated animationIn='zoomIn' animationOut='heartBeat' animationInDuration={3000} animationOutDuration={1000} isVisible={true}>
+                        <img src={image} alt='Pokemon image'/>
+                    </Animated>
+
+                    <img className='pokemon-attribute-ico' src={fire}/>
+                </div>
+
+                <div className='pokemon-id'> 
+                    <p>ID: {id}</p>
+                </div>
+            </div>
+            
+            <div className='pokemon-response-attributes'>
+                <div className='pokemon-response-attributes-container'>
+                    <div className='pokemon-attribute'>
+                        <p><FontAwesomeIcon icon={faHeart}/> HP: {hp}</p>
+                        <progress className={searchState.type} value={hp} max='160'>{searchState.hp}</progress>
+                    </div>
+                    <div className='pokemon-attribute'>
+                        <p><FontAwesomeIcon icon={faRocket}/> Attack: {atk}</p>
+                        <progress value={atk} max='160'></progress>
+                    </div>
+                    <div className='pokemon-attribute'>
+                        <p><FontAwesomeIcon icon={faShieldAlt}/> Defense: {def}</p>
+                        <progress value={def} max='160'></progress>
+                    </div>
+                    <div className='pokemon-attribute'>
+                        <p><FontAwesomeIcon icon={faMeteor}/> Special Attack: {specialAttack}</p>
+                        <progress value={specialAttack} max='160'></progress>
+                    </div>
+                    <div className='pokemon-attribute'>
+                        <p><FontAwesomeIcon icon={faShieldVirus}/> Special Defense: {specialDefense}</p>
+                        <progress value={specialDefense} max='160'></progress>
+                    </div>
+                    <div className='pokemon-attribute'>
+                        <p><FontAwesomeIcon icon={faBolt}/> Speed: {speed}</p>
+                        <progress value={speed} max='160'></progress>
+                    </div>
+                    <div className='pokemon-attribute'>
+                        <p><FontAwesomeIcon icon={faViruses}/> Type: <span className={`${type} pokemon-type`}>{type.charAt(0).toUpperCase() + type.slice(1)}</span></p>
+                    </div>
+                    <div className='pokemon-attribute'>
+                        <p><FontAwesomeIcon icon={faRunning}/> Principal move: {principalMove.charAt(0).toUpperCase() + principalMove.slice(1)}</p>
+                    </div>
+                    <div className='pokemon-attribute'>
+                        <p><FontAwesomeIcon icon={faBomb}/> Principal ability: {ability.charAt(0).toUpperCase() + ability.slice(1)}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ResponseData;
